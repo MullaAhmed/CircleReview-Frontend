d59b612404523c6af9819fbe92db23acd520796e
Dev Parikh <51128342+Dev79844@users.noreply.github.com>
2023-03-16 21:49:24 +0530

    fix: fetchReport

--- src/pages/feed/[id]/index.js
+++ src/pages/feed/[id]/index.js
@@ -7,9 +7,25 @@ const page = () => {
   // const router = useRouter();
   const { query, isReady } = useRouter();
 
+  const {setSurveyQuestions, surveyQuestions} = useContext(AppContext)
+
   if (!isReady) return null;
 
-  return <div>Hello</div>;
+
+  React.useEffect(() => {
+    const getData = async() => {
+      const response = await fetchReport(query.id)
+      setSurveyQuestions(response.questions_answers)
+
+    }
+    getData()
+  },[])
+
+  return <div>{surveyQuestions.map(question => (
+    <div>
+      <p>{question.question}</p>
+    </div>
+  ))}</div>;
 };
 
 export default page;
